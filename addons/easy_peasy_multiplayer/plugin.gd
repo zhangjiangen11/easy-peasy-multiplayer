11d2b18b0619da7e0e70edf34711b53fc260f744
Kj Mumme <kj.mumme@gmail.com>
2025-08-01 11:36:30 -0600

    Added rudimentary dependencies check
    Added a basic filesystem scan and alert dialog to make sure that necessary addon dependencies are present before enabling.

--- addons/easy_peasy_multiplayer/plugin.gd
+++ addons/easy_peasy_multiplayer/plugin.gd
@@ -17,10 +17,24 @@ const SETTINGS: Dictionary = {
 }
 
 func _enable_plugin():
-	# Registers autoloads
-	for autoload in AUTOLOADS:
-		add_autoload_singleton(autoload, AUTOLOADS[autoload])
-	_add_project_settings()
+	var godotsteam_exists := DirAccess.dir_exists_absolute("res://addons/godotsteam/")
+	var multiplayer_peer_exists := DirAccess.dir_exists_absolute("res://addons/steam-multiplayer-peer")
+
+	if godotsteam_exists and multiplayer_peer_exists:
+		# Registers autoloads
+		for autoload in AUTOLOADS:
+			add_autoload_singleton(autoload, AUTOLOADS[autoload])
+		_add_project_settings()
+	else:
+		var dialog := AcceptDialog.new()
+		dialog.title = "Missing Required Dependencies"
+		dialog.dialog_text = "You are missing the following dependencies required for this addon to function: \n"
+		if not godotsteam_exists:
+			dialog.dialog_text += "GodotSteam"
+		if not multiplayer_peer_exists:
+			dialog.dialog_text += "Steam Multiplayer Peer"
+
+		EditorInterface.popup_dialog_centered(dialog)
 
 func _disable_plugin():
 	# Removes autoloads
